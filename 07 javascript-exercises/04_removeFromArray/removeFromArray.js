0562a7fcc0bd09dd36854202839193ab7864a996
pidstudev <pidstudev@gmail.com>
2024-01-25 10:32:35 +0800

    Complete 04_removeFromArray

--- 07 javascript-exercises/04_removeFromArray/removeFromArray.js
+++ 07 javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...value) {
+    return array.filter( item => !value.includes(item) )
 };
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
