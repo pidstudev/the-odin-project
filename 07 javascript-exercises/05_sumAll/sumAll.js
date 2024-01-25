9da16dcba2b39881749a668cbd3b1980242d4b54
pidstudev <pidstudev@gmail.com>
2024-01-25 12:41:02 +0800

    Complete 05_sumAll

--- 07 javascript-exercises/05_sumAll/sumAll.js
+++ 07 javascript-exercises/05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let finalSum = 0;
 
+    if (typeof num1 !== "number" || typeof num2 !== "number") {
+        return "ERROR";
+    } else if (num1 === num2) {
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+
+    const start = Math.min(num1, num2);
+    const end = Math.max(num1, num2);
+
+    for (let i = start; i <= end; i++) {
+        finalSum += i;
+    }
+
+    return finalSum;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
