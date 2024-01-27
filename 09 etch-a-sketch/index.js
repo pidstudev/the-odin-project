214f96619964de209991ade6d72fac3fbfba4911
pidstudev <pidstudev@gmail.com>
2024-01-27 12:42:53 +0800

    Complete etch-a-sketch program. TODO: Improve UI/UX

--- 09 etch-a-sketch/index.js
+++ 09 etch-a-sketch/index.js
@@ -1,6 +1,12 @@
 const divContainer = document.getElementById("div-container");
 let isMouseDown = false;
 
+const gridSize = 64;
+
+function calculateSquareSize() {
+    const drawingPadSize = 400;
+    return drawingPadSize / gridSize;
+}
 
 function changeSquareColor(squareDiv) {
     if (isMouseDown) {
@@ -16,15 +22,19 @@ document.addEventListener('mouseup', function () {
     isMouseDown = false;
 });
 
-for (let i = 1; i <= 16; i++) {
+for (let i = 1; i <= gridSize; i++) {
 
     const row = document.createElement('div');
     row.className = "row";
 
-    for (let j = 1; j <= 16; j++) {
+    for (let j = 1; j <= gridSize; j++) {
 
         const squareDiv = document.createElement('div');
         squareDiv.className = "square";
+
+        const squareSize = calculateSquareSize();
+        squareDiv.style.width = squareSize + "px";
+        squareDiv.style.height = squareSize + "px";
 
         squareDiv.addEventListener('mousedown', function () {
             changeSquareColor(squareDiv);
