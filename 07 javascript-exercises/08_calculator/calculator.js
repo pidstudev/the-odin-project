8d9157a3722374cbbe1a78dff308cb9fcc652b05
pidstudev <pidstudev@gmail.com>
2024-01-27 16:27:09 +0800

    Complete 08_calculator assignment

--- 07 javascript-exercises/08_calculator/calculator.js
+++ 07 javascript-exercises/08_calculator/calculator.js
@@ -1,25 +1,37 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+	return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let result = 0;
+	for (let i = 0; i < array.length; i++) {
+    result += array[i];
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let result = 1;
+  for (let i = 0; i < array.length; i++) {
+    result *= array[i];
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+	return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i >= 1; i--) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
@@ -31,3 +43,4 @@ module.exports = {
   power,
   factorial
 };
+
