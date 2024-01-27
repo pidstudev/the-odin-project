5bb15a78079a15f4a69fb63315d3d4ef4acf72e3
pidstudev <pidstudev@gmail.com>
2024-01-27 18:17:38 +0800

    Complete 12_findTheOldest assignment

--- 07 javascript-exercises/12_findTheOldest/findTheOldest.js
+++ 07 javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,6 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, person) => {
+        const age = (person.yearOfDeath || new Date().getFullYear()) - person.yearOfBirth;
+        const oldestAge = (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth;
+        return age > oldestAge ? person : oldest;
+    }, people[0]);
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
