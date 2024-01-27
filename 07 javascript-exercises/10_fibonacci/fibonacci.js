4eb54dcec5f35916bd6a087683bd9c079cae55be
pidstudev <pidstudev@gmail.com>
2024-01-27 17:26:20 +0800

    Complete 10_fibonacci assignment

--- 07 javascript-exercises/10_fibonacci/fibonacci.js
+++ 07 javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,6 +1,26 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let num = Number(input);
+    
+    if (num === 0) {
+        return 0;
+    } else if (num === 1 || num === 2) {
+        return 1;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+        let a = 1;
+        let b = 1;
+        let result = 0;
 
+        for (let i = 3; i <= num; i++) {
+            result = a + b;
+            a = b;
+            b = result;
+        }
+        return result;
+    }
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
