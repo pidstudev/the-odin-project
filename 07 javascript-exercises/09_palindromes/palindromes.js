9110dbeddec4628db231c9143f8137c2497a6a3f
pidstudev <pidstudev@gmail.com>
2024-01-27 16:56:29 +0800

    Complete 09_palindromes assignment

--- 07 javascript-exercises/09_palindromes/palindromes.js
+++ 07 javascript-exercises/09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let newString = string.replace(/ |,|\.|!|/g, "").toLowerCase();
+    let result = "";
+    for (let i = newString.length - 1; i >= 0; i--) {
+        result += newString[i];
+    }
+    if (result === newString) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
