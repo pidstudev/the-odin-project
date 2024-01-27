ffb8c8e43db74310a01b927c5a5dd93ed8f8e5f9
pidstudev <pidstudev@gmail.com>
2024-01-27 17:35:11 +0800

    Complete 11_getTheTitles assignment

--- 07 javascript-exercises/11_getTheTitles/getTheTitles.js
+++ 07 javascript-exercises/11_getTheTitles/getTheTitles.js
@@ -1,6 +1,11 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let titles = [];
+    for (let i = 0; i < array.length; i++) {
+        titles.push(array[i]['title'])
+    }
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
